9bb0c537a0cbf06d2792241057a379f143aa451a
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2022-06-21 20:43:38 -0300

    create test for tdd

--- registration-product/__tests__/integration/modules/product/create.test.ts
+++ registration-product/__tests__/integration/modules/product/create.test.ts
@@ -4,12 +4,21 @@ import prisma from "../../../../src/shared/config/lib/prisma";
 describe("Product Create", () => {
   it("Should be create a new product", async () => {
     const data = {
+      title: "Produto 1",
+    };
+    const response = await request.post("/product").send(data);
 
-    }
-
-    prisma.product.create({data: {title: "Teste"}})
-    const response = await request.post("/product").send(data)
-    
     expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("product");
+    expect(response.body.product).toHaveProperty("title", "Produto 1");
+  });
+
+  it("Should not be create a new product, because title can not be null", async () => {
+    const data = {
+      title: "",
+    };
+    const response = await request.post("/product").send(data);
+
+    expect(response.status).toBe(422);
   });
 });
