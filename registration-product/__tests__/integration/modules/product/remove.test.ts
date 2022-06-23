ebefa49e73d583d60c0cf737498247d6520a6e74
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2022-06-23 18:26:43 -0300

    create tdd

---
+++ registration-product/__tests__/integration/modules/product/remove.test.ts
@@ -0,0 +1,23 @@
+import { request } from "../../../config/supertest";
+import prisma from "../../../../src/shared/config/lib/prisma";
+
+describe("Product Remove", () => {
+  it("Should be remove a product", async () => {
+    const product = await prisma.product.create({
+      data: {
+        title: "Produto 1",
+      },
+    });
+
+    const response = await request.delete(`/product/${product.id}`).send();
+
+    expect(response.status).toBe(204);
+  });
+
+  it("Should not be remove product, because invalid id", async () => {
+    const response = await request.delete(`/product/${99}`).send();
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe("Product not found");
+  });
+});
