80cf326a6beeabfa7588ebe9eaf2c94d03f1cbbc
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2022-06-23 18:23:51 -0300

    implementation update product

--- registration-product/src/modules/Product/usecases/UpdateProduct.ts
+++ registration-product/src/modules/Product/usecases/UpdateProduct.ts
@@ -1,9 +1,17 @@
 import { IProductDTO } from "../dto/IProductDTO";
 import { AppException } from "@errors/AppException";
-
+import prisma from "@config/lib/prisma";
 export class UpdateProduct {
   public async execute(id: number, title: string): Promise<IProductDTO> {
     if (!id || !title) throw new AppException("Params required", 422);
-    return { title: "Produto Alterado", id, createdAt: new Date(), updatedAt: new Date() };
+    const productUpdated = await prisma.product.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+      },
+    });
+    return productUpdated;
   }
 }
